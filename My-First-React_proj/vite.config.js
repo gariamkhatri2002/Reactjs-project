e16c279b7ef9ea699f021b9850b4555a76814032
Gaurav <khatrigarima073@gmail.com>
2024-12-03 13:01:16 +0530

    changes

--- My-First-React_proj/vite.config.js
+++ My-First-React_proj/vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    base: '/', // Ensure the base path is correct for deployment
+    plugins: [react()], // Ensure the base path is correct for deployment
 });
