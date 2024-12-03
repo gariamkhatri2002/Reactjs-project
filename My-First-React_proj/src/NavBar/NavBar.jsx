b820d2dd28b50aea8cf4b02e87177499d0ba8f64
Gaurav <khatrigarima073@gmail.com>
2024-12-03 11:52:15 +0530

    my portfolio

---
+++ My-First-React_proj/src/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <div>
+            <div className="nav">
+                 <div className="left"><h2>Company Name</h2></div>
+                 <div className="left">
+                    <ul>
+                        <li><a href="">First Item</a></li>
+                        <li><a href="">First Item</a></li>
+                        <li><a href="">First Item</a></li>
+                        <li><a href="">First Item</a></li>
+                        <li><a href="">First Item</a></li>
+                    </ul>
+                 </div>
+            </div>
+    </div>
+  )
+}
+
+export default NavBar
